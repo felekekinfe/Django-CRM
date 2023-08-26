7485f3c6eea87599d9fcdeccd96ec69b5058631b
felekekinfe <felekekinfe@gmail.com>
2023-08-26 02:24:58 -0400

    registration

---
+++ App/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class signUpForm(UserCreationForm):
+    email=forms.EmailField(label='Email',widget=forms.TextInput(), required=True)
+    first_name=forms.CharField(label='FirstName',widget=forms.TextInput(),max_length=100, required=True)
+    last_name= first_name=forms.CharField(label='LastName',widget=forms.TextInput(),max_length=100, required=True)
+
+    class Meta:
+        model=User
+        fields=('username','first_name','last_name','email','password1','password2')
+        
---
+++ App/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+
+
+urlpatterns = [
+    path('home/',views.home, name="home"),
+    # path('login/',views.login_user, name="login"),
+    path('logout/',views.logout_user, name="logout"),
+    path('register/',views.register_user,name='register')
+]
